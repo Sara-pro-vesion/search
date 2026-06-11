147dca9734a242405b7e4bc03670fed98646b843
sara <kr.sara.dev@gmail.com>
2026-06-11 16:12:31 -0700

    results list component

--- src/components/ResultsList.jsx
+++ src/components/ResultsList.jsx
@@ -1,3 +1,23 @@
+import React, { useEffect, useState } from 'react'
+import UserCard from './UserCard'
 
+export default function ResultsList() {
+     const [data,setData] = useState([])
+
+    useEffect(() => {
+       fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(json => setData(json))
+      .catch((error) => console.error("ERROR: " , error));}
+    ,[]);
+
+  return (
+    <div>
+      {data.map((user) => (
+        <UserCard name={user.name} email={user.email} id={user.id}/>
+    ))}
+    </div>
+  )
+}
 
 
