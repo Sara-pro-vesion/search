b86a0c4bcb7bff38858f32483a599bd5a660838b
sara <kr.sara.dev@gmail.com>
2026-06-12 12:54:58 -0700

    searching functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import SearchBar from './components/SearchBar'
 import ResultsList from './components/ResultsList'
-import UserCard from './components/UserCard'
 
 export default function App() {
+
+  const [query ,setQuery] = useState('');
+  const [data ,setData] = useState([]);
+
+  useEffect(() => {
+         fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(json => setData(json))
+        .catch((error) => console.error("ERROR: " , error));
+        console.log("data fetched")}
+      ,[]);
+      
+  const filteredData = query
+    ? data.filter((user) => user.name.toLowerCase().includes(query))
+    : data
+
+  const handleSearch = (text) => {
+    setQuery(text.toLowerCase())
+    console.log('Searching for:', text)
+  }
+
   return (
     <div className='flex flex-col justify-center items-center p-12'>
-      <SearchBar/>
-      <UserCard/>
-      <ResultsList/>
-    </div>
+     
+      <SearchBar onSearch = {handleSearch} /> 
+      <ResultsList users={filteredData} /> 
+      </div>
+
   )
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,22 +1,34 @@
 import React, { useState } from 'react'
 
-export default function SearchBar() {
+export default function SearchBar({ onSearch }) {
   const [input ,setInput] = useState('')
   
   const handleClick= () =>{
     setInput('')
+    if (typeof onSearch === 'function') onSearch('')
   }
+
+    const handleChange = (e) => {
+    const value = e.target.value;
+        setInput(value);
+
+        if (typeof onSearch === 'function') {
+          onSearch(value);
+        } else {
+          console.error("onSearch prop is missing or not a function!");
+        }}
+
     return (
     <div>
-        <form id='searchForm' onSubmit={(e) => e.preventDefault()}>
+        <form id='searchForm' className='mb-5' onSubmit={(e) => e.preventDefault()}>
       <input
       type='text'
       value={input}
       placeholder='Search'
-      className=' w-[220px] border-2 border-[#FFCAE5] p-2 rounded-sm'
-      onChange={(e) => setInput(e.target.value)}
+      className='w-[220px] text-white border-2 border-[#FFCAE5] p-2 rounded-sm'
+      onChange={handleChange}
       ></input>
-      <button className='border-2 border-[#FFCAE5] rounded-sm p-2 ' onClick={handleClick}>🔍</button></form>
+        <button type='button' className='border-2 border-[#FFCAE5] rounded-sm p-2 ' onClick={handleClick}>🔍</button></form>
     </div>
   )
 }
